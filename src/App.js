9094129df85a53393d15069dda05c6291d15e76f
Renahn Schultz <renahnschultz@gmail.com>
2020-03-25 17:35:58 -0300

    Feature: adicionado semantic

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Header from './componentes/Header'
+import 'semantic-ui-css/semantic.min.css'
+import { Form, Container, Button } from 'semantic-ui-react';
+
+const campos = [
+  { label: 'Descrição', ph: 'Descrição da despesa', name: 'descricao' },
+  { label: 'Valor', ph: 'Valor da despesa', name: 'valor' }
+]
+
 
 function App() {
+  console.log(campos)
   return (
-    <Header />
+    <>
+      <Header />
+      <Container fluid className='container'>
+        <Form>
+          <Form.Group>
+            {campos.map(campo => (
+              <Form.Field width={6}>
+                <label>{campo.label}</label>
+                <input placeholder={campo.ph} name={campo.name} />
+              </Form.Field>
+            ))}
+            <Form.Field width={4}>
+              <Button content='Salvar'></Button>
+            </Form.Field>
+          </Form.Group>
+        </Form>
+      </Container>
+    </>
   );
 }
 
